be439fdda6637f82a1810f593ce3a1ba96a03870
Alper Cirak <alprcrk@hotmail.com>
2023-10-30 16:07:10 +0300

    Fibonnaci Update

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number = 100;  // Hesaplanacak Fibonacci sayısı
+        double number1 = 0;  
+        double number2 = 1;  
+
+        Console.WriteLine(number + " " + "Fibonnaci");  // hesaplanan Fibonacci dizisinin eleman sayısı
+
+        for (int i = 0; i < number; i++) 
+        {
+            Console.WriteLine(number1);  
+
+            double next = number1 + number2;  // Bir sonraki Fibonacci terimi hesaplanır ve 'next' değişkenine atanır
+
+            number1 = number2;  // 'number1', 'number2' değerine atanır
+            number2 = next;  // 'number2', bir sonraki terim olan 'next' değerine atanır
+        }
+        
+    }
+}
